252e18d59290ed05a51644635064013a0c6a4ae0
Elliot Tikhomirov <tikhomirovi@icloud.com>
2019-11-16 11:58:13 +1100

    Initial Commit

---
+++ SwiftUI-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Test
+//
+//  Created by Elliot Tikhomirov on 16/11/19.
+//  Copyright © 2019 Elliot Tikhomirov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
